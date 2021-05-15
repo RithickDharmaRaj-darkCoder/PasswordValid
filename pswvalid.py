ad55116617416fa03050c2cd5ee64abd19be4e4f
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-05-16 01:22:59 +0530

    Password checking for both admin and user mode.

--- pswvalid.py
+++ pswvalid.py
@@ -1,15 +1,16 @@
 #variables for password ckecking...
-our_password = "pass123"
+orgin_psw = input("Create new password (Admin!): ")
+max_no_try = int(input("Maximum no.of try to enter the password correctly : "))
+print("\n",f"               WARNING!, You have only {max_no_try} chance to enter the password correctly!")
 ur_password = " "
 no_of_try = 0
-max_no_try = 10
 max_try_cmd = "Not reached the max try!!!"
 
 #checks whether password matches or try reaches the maximum try...
-while ur_password != our_password and max_try_cmd != "Reached the max try":
+while ur_password != orgin_psw and max_try_cmd != "Reached the max try":
     if no_of_try < max_no_try:
-        ur_password = input("Enter the password : ")     #getting password from user...
-        no_of_try = no_of_try + 1               #increment the number of try...
+        ur_password = input(f"Enter the password for {no_of_try}th try : ")     #getting password from user...
+        no_of_try = no_of_try + 1                        #increment the number of try...
     else:
         max_try_cmd = "Reached the max try"
 
@@ -17,4 +18,4 @@ while ur_password != our_password and max_try_cmd != "Reached the max try":
 if max_try_cmd == "Reached the max try":
     print("Account Blocked!, you have wrongly entered the password manytimes.")
 else:
-    print("Password matched! & Access geanted!")
+    print("Password matched! & Access granted!")
